89c91b1c3543f49fa96d750cfa96ed816816bf52
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-07 21:09:47 +0300

    Add Reset Password Form to reset password page

---
+++ src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,20 @@
+import ResetPasswordForm from '@/app/AppComponents/Auth/ResetPasswordForm'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import React from 'react'
+
+export default function ResetPassword() {
+  return (
+    <>
+      <section className="py-36 px-5">
+        <Card className="w-full max-w-sm mx-auto shadow-lg shadow-stone-700 pb-1 ">
+          <CardHeader>
+            <CardTitle className="text-stone-900 text-center">
+              Reset Password
+            </CardTitle>
+          </CardHeader>
+          <ResetPasswordForm />
+        </Card>
+      </section>
+    </>
+  )
+}
