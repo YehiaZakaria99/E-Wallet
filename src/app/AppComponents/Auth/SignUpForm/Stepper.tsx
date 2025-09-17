ceb0e58fd7a7d22ae27f6eeb8e2cd091e77520a4
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-17 15:47:58 +0300

    Create stepper component to track steps of sign up form and add it to card header

---
+++ src/app/AppComponents/Auth/SignUpForm/Stepper.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { cn } from "@/lib/utils"; // لو عندك util للـ classNames
+import { useState } from "react";
+
+type StepperProps = {
+    step: number;
+    steps: number;
+};
+
+export function Stepper({ step, steps }: StepperProps) {
+    return (
+        <div className="flex items-center justify-center ">
+            {Array.from({ length: steps }).map((_, index) => {
+                const currentStep = index + 1;
+                const isActive = step >= currentStep;
+                const isCompleted = step > currentStep;
+
+                return (
+                    <div key={index} className="flex items-center">
+                        {/* Circle */}
+                        <div
+                            className={cn(
+                                "flex items-center justify-center w-10 h-10 rounded-full border-2 transition-colors duration-500",
+                                isActive
+                                    ? "border-stone-900 text-stone-900"
+                                    : "border-stone-400 text-stone-400"
+                            )}
+                        >
+                            {currentStep}
+                        </div>
+
+                        {/* Line */}
+                        {currentStep < steps && (
+                            <div className="relative w-20 h-1 bg-gray-300">
+                                <div
+                                    className={cn(
+                                        "absolute left-0 top-0 h-1 bg-stone-900 transition-all duration-700",
+                                        isCompleted ? "w-full" : "w-0"
+                                    )}
+                                />
+                            </div>
+                        )}
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
