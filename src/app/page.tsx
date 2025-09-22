98da6c318bbb455ce0f760358ef0199e3733d46b
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-22 06:45:01 +0300

    Add Sections to Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-export default function Home() {
+import ChooseUsSection from "./_sections/ChooseUsSection";
+import HeroSection from "./_sections/HeroSection";
+
+export default function HomePage() {
   return (
     <>
-      <section className="hero py-64 " id="hero">
-        <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
-          Welcome To E-Wallet
-        </h1>
+      <section className="hero " id="hero">
+        {/* <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+        </h1> */}
+          <HeroSection />
+          <ChooseUsSection />
+
       </section>
     </>
   );
