021b87ade225759b1a8677c61018778c9d77acba
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-22 08:15:29 +0300

    Edit Links in Navbar and add Accounts, Transactions, and Verification links in navbar

--- src/app/_AppComponents/Navbar/Navbar.tsx
+++ src/app/_AppComponents/Navbar/Navbar.tsx
@@ -10,6 +10,20 @@ const links: linksType[] = [
         name: "Home"
     },
     {
+        href: "/accounts",
+        name: "Accounts"
+    },
+    {
+        href: "/transactions",
+        name: "Transactions"
+    },
+    {
+        href: "/verification",
+        name: "Verification"
+    },
+]
+const auth: linksType[] = [
+    {
         href: "/login",
         name: "Login"
     },
@@ -23,8 +37,8 @@ export default function Navbar() {
     return (
         <>
             <nav className=" border-gray-200 bg-blue-950  py-3 fixed z-[9999] top-0 left-0 w-full ">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <div className="max-w-screen-xl flex flex-wrap items-center  mx-auto p-4">
+                    <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse flex-1">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">E-Wallet</span>
                     </Link>
                     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
@@ -33,11 +47,21 @@ export default function Navbar() {
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
-                    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:border-gray-700">
-                            {links.map((link) => (
-                                <NavbarDesktop key={link.href} link={link} />
-                            ))
+                    <div className="hidden w-full md:flex justify-between md:w-auto mx-auto flex-2 " id="navbar-default">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 md:flex-row  md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:border-gray-700">
+                            {
+                                links.map((link) => (
+                                    <NavbarDesktop key={link.href} link={link} />
+                                ))
+                            }
+                        </ul>
+                        {/* Auth */}
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 md:flex-row  md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:border-gray-700">
+
+                            {
+                                auth.map((link) => (
+                                    <NavbarDesktop key={link.href} link={link} />
+                                ))
                             }
                         </ul>
                     </div>
