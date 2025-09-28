34e41088575e3faa77eb3f76cc8219011dccf0f0
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-28 15:37:24 +0300

    redirect user to dashboard instead of home if he is logged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
+import { cookies } from "next/headers";
 import ChooseUsSection from "./_sections/ChooseUsSection";
 import HeroSection from "./_sections/HeroSection";
+import { redirect } from "next/navigation";
+import { loggedUser } from "./_AppComponents/Guard/loggedUser";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const isLoggedUser = await loggedUser();
+  if (isLoggedUser) {
+    redirect("/dashboard");
+  }
+
   return (
     <>
       <section className="hero " id="hero">
         {/* <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
         </h1> */}
-          <HeroSection />
-          <ChooseUsSection />
+        <HeroSection />
+        <ChooseUsSection />
 
       </section>
     </>
