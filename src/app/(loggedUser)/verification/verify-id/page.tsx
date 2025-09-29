20e7de0aca95fdae03611347740d87ef2195b0a1
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-30 01:17:29 +0300

    Create VerifyIdPage

---
+++ src/app/(loggedUser)/verification/verify-id/page.tsx
@@ -0,0 +1,13 @@
+import VerifyIdCard from '@/app/_AppComponents/Dashboard/Verification/VerifyIdCard'
+import React from 'react'
+
+export default function VerifyIdPage() {
+
+    return (
+        <>
+            <section className='max-w-2xl mx-auto py-48'>
+                <VerifyIdCard />
+            </section>
+        </>
+    )
+}
