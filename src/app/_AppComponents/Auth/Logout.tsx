cf75d4235e8a3d462fbe3942af2be32723cc6c6c
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-30 01:18:25 +0300

    Edit Logout add useRouter instead of redirect

--- src/app/_AppComponents/Auth/Logout.tsx
+++ src/app/_AppComponents/Auth/Logout.tsx
@@ -1,25 +1,32 @@
+"use client";
+
 import { useAppDispatch } from '@/lib/redux/hooks'
 import { logout } from '@/lib/redux/slices/auth/signinSlice'
 import { cn } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function Logout() {
-    const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch();
+    const router = useRouter();
+
+    function handleLogout() {
+        dispatch(logout());
+        router.replace("/login");
+    }
+
     return (
-        <>
-            <li>
-                <button
-                    onClick={() => dispatch(logout())}
-                    className={cn(
-                        `block py-2 px-3 text-gray-200 hover:underline hover:underline-offset-4 hover:text-white font-bold`,
-                        `md:hover:bg-transparent md:border-0  md:p-0`,
-                        `transition-all duration-300 `,
-                        `cursor-pointer`
-                    )}
-                >
-                    Logout
-                </button>
-            </li>
-        </>
-    )
+        <li>
+            <button
+                onClick={handleLogout}
+                className={cn(
+                    "block py-2 px-3 text-gray-200 hover:underline hover:underline-offset-4 hover:text-white font-bold",
+                    "md:hover:bg-transparent md:border-0 md:p-0",
+                    "transition-all duration-300 cursor-pointer"
+                )}
+            >
+                Logout
+            </button>
+        </li>
+    );
 }
