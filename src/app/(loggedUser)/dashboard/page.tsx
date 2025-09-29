0da5a3bca75d0c07a3a6d59f57a3f4f1fc6bca30
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-30 01:15:37 +0300

    Edit dashboard

--- src/app/(loggedUser)/dashboard/page.tsx
+++ src/app/(loggedUser)/dashboard/page.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import Dashboard from '../../_AppComponents/Dashboard/Dashboard'
+
 import { loggedUser } from '../../_AppComponents/Guard/loggedUser';
 import { redirect } from 'next/navigation';
+import Dashboard from '@/app/_AppComponents/Dashboard/Dashboard';
 
 export default async function DashboardPage() {
-    // const isLoggedUser = await loggedUser();
-    // if (!isLoggedUser) {
-    //     redirect("/");
-    // }
+    const isLoggedUser = await loggedUser();
+    if (!isLoggedUser) {
+        redirect("/");
+    }
     return (
         <>
             <Dashboard />
+
         </>
     )
 }
