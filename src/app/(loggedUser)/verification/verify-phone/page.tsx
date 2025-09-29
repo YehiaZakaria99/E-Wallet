cead6c911fd98f9e4cc2e929860da8896b6782d7
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-30 01:17:39 +0300

    Create VerifyPhonePage

---
+++ src/app/(loggedUser)/verification/verify-phone/page.tsx
@@ -0,0 +1,12 @@
+import VerifyPhone from '@/app/_AppComponents/Dashboard/Verification/VerifyPhone/VerifyPhone'
+import React from 'react'
+
+export default function VerifyPhonePage() {
+    return (
+        <>
+            <section className='verify-phone max-w-md mx-auto py-48'>
+                <VerifyPhone />
+            </section>
+        </>
+    )
+}
