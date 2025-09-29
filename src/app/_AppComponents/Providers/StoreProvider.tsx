a0d11c6b1cab7daf04253216403bad23022eb628
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-29 03:48:00 +0300

    Provide App With react query

--- src/app/_AppComponents/Providers/StoreProvider.tsx
+++ src/app/_AppComponents/Providers/StoreProvider.tsx
@@ -3,12 +3,18 @@ import { AppStore, store } from '@/lib/redux/store'
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import Navbar from '../Navbar/Navbar'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 type StoreProviderPropsType = {
     children: React.ReactNode
 }
 
+// Create a client
+const queryClient = new QueryClient()
+
 export default function StoreProvider({ children }: StoreProviderPropsType) {
+
+
     const storeRef = useRef<AppStore>(undefined)
     if (!storeRef.current) {
         // Create the store instance the first time this renders
@@ -18,8 +24,10 @@ export default function StoreProvider({ children }: StoreProviderPropsType) {
     return (
         <>
             <Provider store={storeRef.current}>
-                <Navbar />
-                {children}
+                <QueryClientProvider client={queryClient}>
+                    <Navbar />
+                    {children}
+                </QueryClientProvider>
             </Provider>
         </>
     )
