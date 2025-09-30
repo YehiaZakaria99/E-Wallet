d9e750ecd5299f8bb2fd01abfeb50d7edd5b1eb8
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-30 19:08:26 +0300

    hook for get kny status

---
+++ src/hooks/useKycStatus.ts
@@ -0,0 +1,43 @@
+import { baseUrl } from "@/server/config";
+import { useQuery } from "@tanstack/react-query";
+import Cookies from "js-cookie";
+
+type successFinalRespType = {
+  message: string;
+};
+
+export function useKycStatus() {
+  const token = Cookies.get("userToken");
+
+  return useQuery({
+    queryKey: ["kycStatus"],
+    queryFn: async () => {
+      const res = await fetch(`${baseUrl}/auth/kyc/status`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      let finalResp;
+      try {
+        finalResp = await res.json();
+      } catch {
+        throw new Error("Verification failed");
+      }
+
+      if (!res.ok) {
+        throw new Error(
+          Array.isArray(finalResp.message)
+            ? finalResp.message.join(", ")
+            : finalResp.message || "Something went wrong"
+        );
+      }
+
+      console.log(finalResp);
+
+      return finalResp as successFinalRespType;
+    },
+    enabled: !!token,
+  });
+}
