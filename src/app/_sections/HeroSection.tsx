1f594bcd3efadf0b4dce2d10ab44ab51d96c6487
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-30 19:08:10 +0300

    fillOpacity

--- src/app/_sections/HeroSection.tsx
+++ src/app/_sections/HeroSection.tsx
@@ -44,7 +44,7 @@ export default function HeroSection() {
             </div>
             <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-[0]">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200">
-                    <path fill="#fff" fill-opacity="1" d="M0,128L80,138.7C160,149,320,171,480,160C640,149,800,107,960,85.3C1120,64,1280,64,1360,64L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+                    <path fill="#fff" fillOpacity="1" d="M0,128L80,138.7C160,149,320,171,480,160C640,149,800,107,960,85.3C1120,64,1280,64,1360,64L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
                 </svg>
             </div>
         </section>
